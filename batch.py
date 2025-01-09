ec51518c7731855cf8fa5a7686dbcc28b8c4d905
Igor <isburenkov@gmail.com>
2025-01-09 18:12:55 +0300

    Переход на ручное считывание диапазонов номеров
    Отказ от работы с Tesseract

--- batch.py
+++ batch.py
@@ -1,71 +1,12 @@
 import json
-import cv2
-from docx import Document
-from docx.shared import Inches
-from PIL import Image
-import traceback
-from visual import PSM3
-import config
 from config import workdir, number
 
 
-print("Processing images...")
-# images = []
-doc = Document()
 problem_ranges: dict[int, tuple[int, int]] = {}
 # for i in range(1, 2):
 for i in range(1, number + 1):
-    src_img_path = f"{workdir}/src/{str(i).zfill(2)}.png"
-    img = cv2.imread(src_img_path)
+    problem_ranges[i] = (0, 0)
 
-    with open(f"{workdir}/psm3/{str(i).zfill(2)}.json", "r") as f:
-        d: dict[str, list[int|float|str]] = json.load(f)
-    
-    try:
-        problems, err = PSM3(img, d)
-    except Exception:
-        traceback.print_exc()
-
-    if not err:
-        # Упрощённый подход
-        numbers = [p.number for p in problems]
-        problem_ranges[i] = (min(numbers), max(numbers))
-    else:
-        problem_ranges[i] = (0, 0)
-    # if problems.check_sequence():
-    #     numbers = [p.number for p in problems]
-    #     problem_ranges[i] = (min(numbers), max(numbers))
-    # else:
-    #     problem_ranges[i] = (0, 0)
-
-
-    # doc.add_picture(src_img_path, width = Inches(6.0))
-
-    # print(problems)
-
-    # for p in problems:
-        # https://stackoverflow.com/questions/9084609/how-to-copy-a-image-region-using-opencv-in-python
-        # if p.Rect is None:
-        #     raise Exception("Problem Rect not initialized")
-        # if p.extra:
-        #     continue
-        # p.statement_img = img[p.Rect.y:p.Rect.v, p.Rect.x:p.Rect.u]
-        # zfilled_number = str(p.number).zfill(3)
-        # tmp_path = f"./Temp/{zfilled_number}.png"
-        # cv2.imwrite(tmp_path, p.statement_img)
-        # doc.add_picture(tmp_path, width = Inches(6.0))
-        # sol_path = f"./solutions/{zfilled_number}.png"
-        # doc.add_picture(sol_path, width = Inches(6.0))
-
-    # images.append(Image.open(f"./Temp/{str(i).zfill(2)}.jpg"))
-    
-    print("#", end = "")
-
-print()
 json_object = json.dumps(problem_ranges, indent=4)
 with open(f"{workdir}/problem_ranges.json", "w") as outfile:
     outfile.write(json_object)
-# doc.save("output.docx")
-# images[0].save(
-#     "output.pdf", "PDF", resolution = 100.0, save_all=True, append_images = images[1:]
-# )
--- config.py
+++ config.py
@@ -1,15 +1,7 @@
 from pathlib import Path
-import tomli_w as tomliw
 
-workdir = Path("./projects/luk-electro")
+workdir = Path("./projects/luk-osc")
 number = len(list((workdir / Path("src")).glob("*.png"))) # Hopefully you didn't put anything unnecessary there
 
 sol_img_ext = "png"
-# atan
-# problem_number_re_str = r'^\d{3}\S?$'
-# luk-stat
-# doc = {"problem_number_re_str": r'^\d{3}\.?\S?$'}
-# luk-electro
-doc = {"problem_number_re_str": r'^\d{4}\.?\S?$'}
-with open(Path("./luk/config.toml"), "wb") as f:
-    tomliw.dump(doc, f)
+
